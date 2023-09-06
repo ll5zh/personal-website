adcaa97e269cc3786210ae84e7a2ab3f958bf99c
Lucia <122565245+ll5zh@users.noreply.github.com>
2023-09-06 16:36:29 -0400

    Add skills in experience section

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ for (let i = 0; i < sections.length; ++i) {
     }
 }
 
+// About Section //
+
 const about = [
     ['Name', 'Lucia Zhang'],
     ['Pronouns', 'she/her'],
@@ -36,3 +38,78 @@ function populateAbout() {
 }
 
 populateAbout();
+
+// Experience Section //
+
+const generalExp = document.querySelector('#general-exp');
+const webExp = document.querySelector('#web-exp');
+const scriptExp = document.querySelector('#script-exp');
+
+const Skill = (name, icon) => {
+    return { name, icon };
+};
+
+const Skills = (() => {
+    const general = [
+        Skill('Python', 'images/python.svg'),
+        Skill('Racket', 'images/racket.svg'),
+    ];
+    const web = [
+        Skill('HTML', 'images/html.svg'),
+        Skill('CSS', 'images/css.svg'),
+        Skill('JavaScript', 'images/javascript.svg'),
+    ];
+    const script = [
+        Skill('Bash', 'images/bash.svg'),
+        Skill('Perl', 'images/perl.svg'),
+    ];
+    const printSkills = () => {
+        general.forEach((item, index) => {
+            // create a button with formatting of name + img
+            const skillBtn = document.createElement('button');
+            skillBtn.innerHTML = item.name;
+            const icon = document.createElement('img');
+            icon.src = item.icon;
+            icon.style.cssText = 'height: 3rem';
+            skillBtn.appendChild(icon);
+            // remember to incorporate flex wrap styling in css
+            // alternate colours (pink/green) given modulo 2
+            skillBtn.style.cssText = 'background-color: #FFB7B7';
+            generalExp.appendChild(skillBtn);
+        });
+        web.forEach((item) => {
+            // create a button with formatting of name + img
+            const skillBtn = document.createElement('button');
+            skillBtn.innerHTML = item.name;
+            const icon = document.createElement('img');
+            icon.src = item.icon;
+            icon.style.cssText = 'height: 3rem';
+            skillBtn.appendChild(icon);
+            // remember to incorporate flex wrap styling in css
+            // alternate colours (pink/green) given modulo 2
+            skillBtn.style.cssText = 'background-color: #96C291';
+            webExp.appendChild(skillBtn);
+        });
+        script.forEach((item) => {
+            // create a button with formatting of name + img
+            const skillBtn = document.createElement('button');
+            skillBtn.innerHTML = item.name;
+            const icon = document.createElement('img');
+            icon.src = item.icon;
+            icon.style.cssText = 'width: 3rem';
+            skillBtn.appendChild(icon);
+            // remember to incorporate flex wrap styling in css
+            // alternate colours (pink/green) given modulo 2
+            skillBtn.style.cssText = 'background-color: #FFB7B7';
+            scriptExp.appendChild(skillBtn);
+        });
+    };
+    return {
+        general,
+        web,
+        script,
+        printSkills,
+    };
+})();
+
+Skills.printSkills();
